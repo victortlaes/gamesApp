c0e53f60fcdd8af983024529253c62f26df78259
Victor Tlaes <94009032+victortlaes@users.noreply.github.com>
2023-12-23 02:48:17 -0300

    Add files via upload

--- src/gamesApp.js
+++ src/gamesApp.js
@@ -2,9 +2,26 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import controleImg from './utils/CONTROLE.png'
 
+import 'material-icons/iconfont/material-icons.css';
+
+
+
 export default function GamesApp() {
   const [games, setGames] = useState([]);
+  const [search, setSearch] = useState('');
   const GAMES_URL = "http://localhost:3005/games";
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(GAMES_URL, { search });
+
+      setGames(response.data);
+    } catch (error) {
+      console.error('Erro na pesquisa:', error);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,34 +42,30 @@ export default function GamesApp() {
       <p>Nao funfou</p>
     </div> :
     <div>
-      {/* <ul>
-        {games && games.map((game) => (
-          <li key={game.id}>
-            <h2>{game.name}</h2>
-            {game.cover && game.cover.url ? (
-              <img src={game.cover.url} alt={game.name} />
-            ) : (
-                <img src={controleImg} alt=""/>
-            )}
-          </li>
-        ))}
-      </ul> */}
       <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">Lista de jogos</h2>
+        <form className="bg-zinc-300 rounded-lg" onSubmit={handleSearch}> 
+                <input className="bg-zinc-300 rounded-lg" id="gameInsert" type="search" placeholder=" Insira o jogo:" value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                />
+                <button type="submit"><span className="material-icons">search</span></button>
+              </form>
+           
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {games && games.map((game) => (
             <div key={game.id} className="group relative">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
-                  src={game.cover && game.cover.url}
+                  src={game.cover && game.cover.url.replace("thumb", "1080p")}
                   alt={game && game.name}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  
+                  className="h-40 w-40 object-cover object-center lg:h-full lg:w-full"
                 />
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
-                  <h3 className="text-sm text-gray-700">:
+                  <h3 className="text-sm text-gray-700">
                     <a href={games.cover && game.cover.url}>
                       <span className="absolute inset-0" />
                       {game.genres && game.genres[0].name}
